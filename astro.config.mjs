8e53917a0f7f5dbc5d124e341b61d774f96e4f47
Roger Collado <roger.collado1130@gmail.com>
2024-08-06 10:25:47 +0200

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'en',
-        locales: ['en', 'es', 'ca'],
-        routing: {
-            prefixDefaultLocale: true,
-        },
-    },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es', 'ca'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  },
+  integrations: [icon()]
 });
---
+++ src/configs/in-development.ts
@@ -0,0 +1,20 @@
+export const inDevelopmentContent = {
+    es: {
+        title: 'En desarrollo',
+        header: 'Lo sentimos! Estamos en desarrollo',
+        message:
+            'Estamos desarrollando nuestra página web, para cualquier duda o información estamos disponibles en nuestras redes sociales:',
+    },
+    ca: {
+        title: 'Pàgina en desenvolupament',
+        header: 'Ho sentim! Estem en desenvolupament',
+        message:
+            'Estem desenvolupant la nostra pàgina web, si teniu qualsevol dubte o consulta estem disponibles a les nostres xarxes socials:',
+    },
+    en: {
+        title: 'Site in Progress',
+        header: "We're sorry! The site is in development",
+        message:
+            'We are currently developing our website, please wait a little bit more! We are available if you need to contact us in our socials:',
+    },
+};
