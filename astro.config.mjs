64942de845942bb3ed500a8bdd92470d06f711ff
Roger Collado <roger.collado1130@gmail.com>
2024-08-07 17:15:10 +0200

    finish in development page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['en', 'es', 'ca'],
-    },
-    integrations: [icon()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['en', 'es', 'ca']
+  },
+  integrations: [icon(), react()]
 });
